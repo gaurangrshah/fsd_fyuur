a595a8ae30c7054012c95b965342e58aebbcf9b1
gaurangrshah <gaurang.r.shah@gmail.com>
2020-04-07 13:17:33 -0400

    extracts enums

--- forms.py
+++ forms.py
@@ -2,82 +2,8 @@ from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField
 from wtforms.validators import DataRequired, AnyOf, URL, Regexp
-
-state_choices = [
-    ('AL', 'AL'),
-    ('AK', 'AK'),
-    ('AZ', 'AZ'),
-    ('AR', 'AR'),
-    ('CA', 'CA'),
-    ('CO', 'CO'),
-    ('CT', 'CT'),
-    ('DE', 'DE'),
-    ('DC', 'DC'),
-    ('FL', 'FL'),
-    ('GA', 'GA'),
-    ('HI', 'HI'),
-    ('ID', 'ID'),
-    ('IL', 'IL'),
-    ('IN', 'IN'),
-    ('IA', 'IA'),
-    ('KS', 'KS'),
-    ('KY', 'KY'),
-    ('LA', 'LA'),
-    ('ME', 'ME'),
-    ('MT', 'MT'),
-    ('NE', 'NE'),
-    ('NV', 'NV'),
-    ('NH', 'NH'),
-    ('NJ', 'NJ'),
-    ('NM', 'NM'),
-    ('NY', 'NY'),
-    ('NC', 'NC'),
-    ('ND', 'ND'),
-    ('OH', 'OH'),
-    ('OK', 'OK'),
-    ('OR', 'OR'),
-    ('MD', 'MD'),
-    ('MA', 'MA'),
-    ('MI', 'MI'),
-    ('MN', 'MN'),
-    ('MS', 'MS'),
-    ('MO', 'MO'),
-    ('PA', 'PA'),
-    ('RI', 'RI'),
-    ('SC', 'SC'),
-    ('SD', 'SD'),
-    ('TN', 'TN'),
-    ('TX', 'TX'),
-    ('UT', 'UT'),
-    ('VT', 'VT'),
-    ('VA', 'VA'),
-    ('WA', 'WA'),
-    ('WV', 'WV'),
-    ('WI', 'WI'),
-    ('WY', 'WY'),
-]
-
-genres_choices = [
-    ('Alternative', 'Alternative'),
-    ('Blues', 'Blues'),
-    ('Classical', 'Classical'),
-    ('Country', 'Country'),
-    ('Electronic', 'Electronic'),
-    ('Folk', 'Folk'),
-    ('Funk', 'Funk'),
-    ('Hip-Hop', 'Hip-Hop'),
-    ('Heavy Metal', 'Heavy Metal'),
-    ('Instrumental', 'Instrumental'),
-    ('Jazz', 'Jazz'),
-    ('Musical Theatre', 'Musical Theatre'),
-    ('Pop', 'Pop'),
-    ('Punk', 'Punk'),
-    ('R&B', 'R&B'),
-    ('Reggae', 'Reggae'),
-    ('Rock n Roll', 'Rock n Roll'),
-    ('Soul', 'Soul'),
-    ('Other', 'Other'),
-]
+from enums import state_choices, genre_choices
+# from enums import State, Genre # see alternate implementation
 
 
 class VenueForm(Form):
@@ -103,8 +29,8 @@ class VenueForm(Form):
     )
     genres = SelectMultipleField(
         # ✅ TODO implement enum restriction
-        'genres', validators=[DataRequired(), AnyOf(genres_choices)],
-        choices=genres_choices
+        'genres', validators=[DataRequired(), AnyOf(genre_choices)],
+        choices=genre_choices
     )
     facebook_link = StringField(
         'facebook_link', validators=[URL()]
@@ -135,9 +61,13 @@ class ArtistForm(Form):
     )
     genres = SelectMultipleField(
         # ✅ TODO implement enum restriction
-        'genres', validators=[DataRequired(), AnyOf(genres_choices)],
-        choices=genres_choices
+        'genres', validators=[DataRequired(), AnyOf(genre_choices)],
+        choices=genre_choices
     )
+    ''''
+    'genres', validators=[DataRequired(), AnyOf( [ (choice.value) for choice in Genre ] )],
+    choices=Genre.choices()
+    '''
     facebook_link = StringField(
         # TODO implement enum restriction
         'facebook_link', validators=[URL()]
