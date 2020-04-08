442c659d6eaeea5e5105ac32c2024f32f1d0ee76
gaurangrshah <gaurang.r.shah@gmail.com>
2020-04-08 11:26:39 -0400

    add genre to venue model

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ class Venue(db.Model):
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     # ✅ TODO: implement any missing fields, as a database migration using Flask-Migrate
